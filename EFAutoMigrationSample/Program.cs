bee311504f2b293f583da74e190768e56fc578f7
jsakamoto <junichi.jp@gmail.com>
2014-11-07 23:15:23 +0900

    Enable automatic migrations.

--- EFAutoMigrationSample/Program.cs
+++ EFAutoMigrationSample/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EFAutoMigrationSample.Migrations;
 using Newtonsoft.Json;
-using System.Data.Entity;
 
 public class User
 {
     public int UserId { get; set; }
     public string Name { get; set; }
-
+    public int Age { get; set; }
+    [Required]
+    public string Note { get; set; }
     public override string ToString() { return JsonConvert.SerializeObject(this); }
 }
 
@@ -24,6 +28,7 @@ class Program
     static void Main(string[] args)
     {
         AppDomain.CurrentDomain.SetData("DataDirectory", AppDomain.CurrentDomain.BaseDirectory);
+        Database.SetInitializer(new MigrateDatabaseToLatestVersion<MyDb, Configuration>());
 
         var db = new MyDb();
 
